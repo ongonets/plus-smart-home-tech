8737849649b5bcde29509260df7cebfa7fd0a324
ongonets <fybiby.2@yandex.ru>
2024-11-28 16:39:45 +0300

    Add SensorEvent

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/model/sensorEvent/LightSensorEvent.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.model.sensorEvent;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class LightSensorEvent extends SensorEvent{
+
+    @NotNull
+    private int linkQuality;
+
+    @NotNull
+    private int luminosity;
+
+    @Override
+    public SensorEventType getType() {
+        return SensorEventType.LIGHT_SENSOR_EVENT;
+    }
+}
