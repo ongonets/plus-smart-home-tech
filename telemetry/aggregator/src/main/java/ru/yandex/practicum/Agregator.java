f48c23df76a84fe1f424e249ea69e34bdba43292
ongonets <fybiby.2@yandex.ru>
2024-12-14 14:17:16 +0300

    Add Dependency

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/Agregator.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agregator {
+    public static void main(String[] args) {
+        SpringApplication.run(Agregator.class, args);
+    }
+}
