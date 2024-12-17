ca34650482b1b8a486bcfca186ac54e5f69366fd
ongonets <fybiby.2@yandex.ru>
2024-12-17 13:08:12 +0300

    Refactor repository

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/repository/ActionRepository.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/repository/ActionRepository.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.model.Action;
+import ru.yandex.practicum.model.Scenario;
 
 public interface ActionRepository extends JpaRepository<Action, Long> {
+
+    void deleteByScenario(Scenario scenario);
 }
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/repository/ConditionRepository.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/repository/ConditionRepository.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.model.Condition;
+import ru.yandex.practicum.model.Scenario;
 
 public interface ConditionRepository extends JpaRepository<Condition, Long> {
+
+    void deleteByScenario(Scenario scenario);
 }
