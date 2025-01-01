33aa4d375547207e2fe24255d191ddd464ec76ba
ongonets <fybiby.2@yandex.ru>
2025-01-01 15:33:40 +0300

    Add ProductDto

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/ProductDto.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ProductDto {
+
+    @NotBlank
+    private String productId;
+
+    @NotBlank
+    private String productName;
+
+    @NotBlank
+    private String description;
+
+    private String imageSrc;
+
+    @NotNull
+    private QuantityState quantityState;
+
+    @NotNull
+    private ProductState productState;
+
+    @NotNull
+    @Min(1)
+    @Max(5)
+    private int rating;
+
+    @NotNull
+    private ProductCategory productCategory;
+
+    @NotNull
+    private double price;
+}
