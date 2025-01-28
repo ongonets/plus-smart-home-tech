bb6d7c67f20076feee6a01c96da0b4fc0ed17d3b
ongonets <fybiby.2@yandex.ru>
2025-01-28 16:13:36 +0300

    Add PaymentController

---
+++ commerce/payment/src/main/java/ru/yandex/practicum/controller/PaymentController.java
@@ -0,0 +1,49 @@
+package ru.yandex.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.dto.OrderDto;
+import ru.yandex.practicum.dto.PaymentDto;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/payment")
+@Slf4j
+@RequiredArgsConstructor
+public class PaymentController {
+
+
+    @PostMapping
+    public PaymentDto createPayment(@RequestBody OrderDto order) {
+        log.info("Received request to create payment for order ID: {}", order.getOrderId());
+        return null;
+    }
+
+    @PostMapping("/totalCost")
+    public Double calculateTotalCost(@RequestBody OrderDto order) {
+        log.info("Received request to calculate total cost for order ID: {}", order.getOrderId());
+        return null;
+    }
+
+    @PostMapping("/refund")
+    public void refundPayment(@RequestBody UUID paymentID) {
+        log.info("Received request to refund payment ID: {}", paymentID);
+    }
+
+    @PostMapping("/productCost")
+    public Double calculateProductCost(@RequestBody OrderDto order) {
+        log.info("Received request to calculate product cost for order ID: {}", order.getOrderId());
+        return null;
+    }
+
+    @PostMapping("/failed")
+    public void failedPayment(@RequestBody UUID paymentID) {
+        log.info("Received request to failed payment ID: {}", paymentID);
+    }
+
+}
